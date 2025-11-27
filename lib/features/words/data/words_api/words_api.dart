2eccd3a2c1f69e6e6b3650a98f81fc8754821bbd
g.sorokin <homyku408@gmail.com>
2025-11-27 17:41:52 +0500

    fix: one more place where adding WordFormData

--- lib/features/words/data/words_api/words_api.dart
+++ lib/features/words/data/words_api/words_api.dart
@@ -1,13 +1,14 @@
 import 'package:english_training_app/features/words/domain/entities/entities.dart';
+import 'package:english_training_app/features/words/domain/entities/word_form_data.dart';
 
 abstract class WordsApi {
   WordsApi();
 
   Stream<List<Word>> watchWordsByDictId(int dictionaryId);
 
-  Future<void> saveWord(Word word);
+  Future<void> saveWord(WordFormData data);
 
-  Future<void> updateWord(Word word);
+  Future<void> updateWord(WordFormData data);
 
   Future<Word> getWordById(int id);
 
